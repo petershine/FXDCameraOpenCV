f4f5ca59a6bcacc3c3fd0668c4b86ad8d9dcddce
petershine <petershine@fxceed.com>
2015-10-05 19:07:31 -0400

    ADDED capturePreview subclass

---
+++ DetectiveCam/DTCpreviewCapture.swift
@@ -0,0 +1,30 @@
+//
+//  DTCpreviewCapture.swift
+//  DetectiveCam
+//
+//  Created by petershine on 10/5/15.
+//  Copyright © 2015 fXceed. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+import AVFoundation;
+
+
+class DTCpreviewCapture : UIView {
+
+	override class func layerClass() -> AnyClass {
+		return AVCaptureVideoPreviewLayer.classForCoder()
+	}
+
+
+	var session : AVCaptureSession {
+		get {
+			return (self.layer as? AVCaptureVideoPreviewLayer)!.session
+		}
+		set (newSession) {
+			(self.layer as? AVCaptureVideoPreviewLayer)!.session = newSession
+		}
+	}
+}
