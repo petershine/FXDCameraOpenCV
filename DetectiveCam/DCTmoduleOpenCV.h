3ae7393583b18bd76bcd3b0bbc42bf88e7f9df0a
petershine <petershine@fxceed.com>
2015-10-05 21:52:11 -0400

    ADDED opencv

---
+++ DetectiveCam/DCTmoduleOpenCV.h
@@ -0,0 +1,13 @@
+//
+//  DCTmoduleOpenCV.h
+//  DetectiveCam
+//
+//  Created by petershine on 10/5/15.
+//  Copyright © 2015 fXceed. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DCTmoduleOpenCV : NSObject
+
+@end
---
+++ DetectiveCam/DCTmoduleOpenCV.m
@@ -0,0 +1,13 @@
+//
+//  DCTmoduleOpenCV.m
+//  DetectiveCam
+//
+//  Created by petershine on 10/5/15.
+//  Copyright © 2015 fXceed. All rights reserved.
+//
+
+#import "DCTmoduleOpenCV.h"
+
+@implementation DCTmoduleOpenCV
+
+@end
