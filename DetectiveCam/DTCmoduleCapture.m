e1753c41589c72cc7615a4291d92f15618ac7427
petershine <petershine@fxceed.com>
2017-01-10 21:53:46 -0500

    FIXED privacy requirement for camera capturing

--- DetectiveCam/DTCmoduleCapture.m
+++ DetectiveCam/DTCmoduleCapture.m
@@ -35,6 +35,10 @@
 		}
 
 
+		//MARK: This app has crashed because it attempted to access privacy-sensitive data without a usage description.  The app's Info.plist must contain an NSCameraUsageDescription key with a string value explaining to the user how the app uses this data.
+
+
+
 		[AVCaptureDevice
 		 requestAccessForMediaType:AVMediaTypeVideo
 		 completionHandler:^(BOOL granted) {
